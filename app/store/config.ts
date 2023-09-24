a5346b9cd16db44c881bcddb9fddfc7cf43e4ee3
supperrzz <jamell.jones@mobelux.com>
2023-09-24 09:35:39 -0700

    fix mobile view

--- app/store/config.ts
+++ app/store/config.ts
@@ -29,9 +29,9 @@ export const DEFAULT_CONFIG = {
 
   submitKey: SubmitKey.CtrlEnter as SubmitKey,
   avatar: "1f603",
-  fontSize: 16,
+  fontSize: 14,
   theme: Theme.Auto as Theme,
-  tightBorder: true,
+  tightBorder: !!getClientConfig()?.isApp,
   sendPreviewBubble: true,
   enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
