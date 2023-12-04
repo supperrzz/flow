0bc1939ab9a614b9a05ae1ff02418ac116710af6
supperrzz <jamell.jones@mobelux.com>
2023-12-03 17:52:45 -0800

    fix import

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -3,7 +3,6 @@ import MARKETING_ACTIONS from "./marketing";
 import BUSINESS_ACTIONS from "./business";
 import BLOG_ACTIONS from "./blog";
 import PRODUCT_ACTIONS from "./product";
-import MESSAGING_ACTIONS from "./messaging";
 import NEWSLETTER_ACTIONS from "./newsletter";
 import WRITING_ACTIONS from "./grammer";
 export default {
@@ -11,8 +10,7 @@ export default {
   ...MARKETING_ACTIONS,
   ...BUSINESS_ACTIONS,
   ...BLOG_ACTIONS,
-  // ...PRODUCT_ACTIONS,
-  ...MESSAGING_ACTIONS,
+  ...PRODUCT_ACTIONS,
   ...NEWSLETTER_ACTIONS,
   ...WRITING_ACTIONS,
 };
--- app/config/actions.ts
+++ app/config/actions.ts
@@ -4,7 +4,7 @@ import MARKETING_ACTIONS from "../actions/marketing";
 import BUSINESS_ACTIONS from "../actions/business";
 import BLOG_ACTIONS from "../actions/blog";
 import GRAMMER_ACTIONS from "../actions/grammer";
-// import PRODUCT_ACTIONS from "../actions/product";
+import PRODUCT_ACTIONS from "../actions/product";
 import NEWSLETTER_ACTIONS from "../actions/newsletter";
 
 type Actions = {
@@ -17,7 +17,7 @@ const ACTIONS: Actions = {
   business: BUSINESS_ACTIONS,
   blog: BLOG_ACTIONS,
   writing: GRAMMER_ACTIONS,
-  // product: PRODUCT_ACTIONS,
+  product: PRODUCT_ACTIONS,
   course: COURSE_ACTIONS,
   marketing: MARKETING_ACTIONS,
   newsletter: NEWSLETTER_ACTIONS,
