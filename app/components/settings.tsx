dd007040e22d9b4a6c44d05aee8ea880e55c9445
Jamell Jones <jamell@goagento.com>
2025-07-17 23:56:22 -0700

    update settings component to disable access code display and clean up commented code

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -636,7 +636,9 @@ export function Settings() {
   }, []);
 
   const clientConfig = useMemo(() => getClientConfig(), []);
-  const showAccessCode = enabledAccessControl && !clientConfig?.isApp;
+  // const showAccessCode = enabledAccessControl && !clientConfig?.isApp;
+  // Don't show access code for now
+  const showAccessCode = false;
 
   const accessCodeComponent = showAccessCode && (
     <ListItem
@@ -656,63 +658,63 @@ export function Settings() {
     </ListItem>
   );
 
-  // const useCustomConfigComponent = // Conditionally render the following ListItem based on clientConfig.isApp
-  //   !clientConfig?.isApp && ( // only show if isApp is false
-  //     <ListItem
-  //       title={Locale.Settings.Access.CustomEndpoint.Title}
-  //       subTitle={Locale.Settings.Access.CustomEndpoint.SubTitle}
-  //     >
-  //       <input
-  //         aria-label={Locale.Settings.Access.CustomEndpoint.Title}
-  //         type="checkbox"
-  //         checked={accessStore.useCustomConfig}
-  //         onChange={(e) =>
-  //           accessStore.update(
-  //             (access) => (access.useCustomConfig = e.currentTarget.checked),
-  //           )
-  //         }
-  //       ></input>
-  //     </ListItem>
-  //   );
+  const useCustomConfigComponent = // Conditionally render the following ListItem based on clientConfig.isApp
+    !clientConfig?.isApp && ( // only show if isApp is false
+      <ListItem
+        title={Locale.Settings.Access.CustomEndpoint.Title}
+        subTitle={Locale.Settings.Access.CustomEndpoint.SubTitle}
+      >
+        <input
+          aria-label={Locale.Settings.Access.CustomEndpoint.Title}
+          type="checkbox"
+          checked={accessStore.useCustomConfig}
+          onChange={(e) =>
+            accessStore.update(
+              (access) => (access.useCustomConfig = e.currentTarget.checked),
+            )
+          }
+        ></input>
+      </ListItem>
+    );
 
-  // const openAIConfigComponent = accessStore.provider ===
-  //   ServiceProvider.OpenAI && (
-  //   <>
-  //     <ListItem
-  //       title={Locale.Settings.Access.OpenAI.Endpoint.Title}
-  //       subTitle={Locale.Settings.Access.OpenAI.Endpoint.SubTitle}
-  //     >
-  //       <input
-  //         aria-label={Locale.Settings.Access.OpenAI.Endpoint.Title}
-  //         type="text"
-  //         value={accessStore.openaiUrl}
-  //         placeholder={OPENAI_BASE_URL}
-  //         onChange={(e) =>
-  //           accessStore.update(
-  //             (access) => (access.openaiUrl = e.currentTarget.value),
-  //           )
-  //         }
-  //       ></input>
-  //     </ListItem>
-  //     <ListItem
-  //       title={Locale.Settings.Access.OpenAI.ApiKey.Title}
-  //       subTitle={Locale.Settings.Access.OpenAI.ApiKey.SubTitle}
-  //     >
-  //       <PasswordInput
-  //         aria={Locale.Settings.ShowPassword}
-  //         aria-label={Locale.Settings.Access.OpenAI.ApiKey.Title}
-  //         value={accessStore.openaiApiKey}
-  //         type="text"
-  //         placeholder={Locale.Settings.Access.OpenAI.ApiKey.Placeholder}
-  //         onChange={(e) => {
-  //           accessStore.update(
-  //             (access) => (access.openaiApiKey = e.currentTarget.value),
-  //           );
-  //         }}
-  //       />
-  //     </ListItem>
-  //   </>
-  // );
+  const openAIConfigComponent = accessStore.provider ===
+    ServiceProvider.OpenAI && (
+    <>
+      {/* <ListItem
+        title={Locale.Settings.Access.OpenAI.Endpoint.Title}
+        subTitle={Locale.Settings.Access.OpenAI.Endpoint.SubTitle}
+      >
+        <input
+          aria-label={Locale.Settings.Access.OpenAI.Endpoint.Title}
+          type="text"
+          value={accessStore.openaiUrl}
+          placeholder={OPENAI_BASE_URL}
+          onChange={(e) =>
+            accessStore.update(
+              (access) => (access.openaiUrl = e.currentTarget.value),
+            )
+          }
+        ></input>
+      </ListItem> */}
+      <ListItem
+        title={Locale.Settings.Access.OpenAI.ApiKey.Title}
+        subTitle={Locale.Settings.Access.OpenAI.ApiKey.SubTitle}
+      >
+        <PasswordInput
+          aria={Locale.Settings.ShowPassword}
+          aria-label={Locale.Settings.Access.OpenAI.ApiKey.Title}
+          value={accessStore.openaiApiKey}
+          type="text"
+          placeholder={Locale.Settings.Access.OpenAI.ApiKey.Placeholder}
+          onChange={(e) => {
+            accessStore.update(
+              (access) => (access.openaiApiKey = e.currentTarget.value),
+            );
+          }}
+        />
+      </ListItem>
+    </>
+  );
 
   // const azureConfigComponent = accessStore.provider ===
   //   ServiceProvider.Azure && (
@@ -1260,6 +1262,72 @@ export function Settings() {
         </div>
       </div>
       <div className={styles["settings"]}>
+                <List id={SlotID.CustomModel}>
+          {accessCodeComponent}
+
+          {!accessStore.hideUserApiKey && (
+            <>
+              {/* {useCustomConfigComponent} */}
+
+              {
+                <>
+                  {/* <ListItem
+                    title={Locale.Settings.Access.Provider.Title}
+                    subTitle={Locale.Settings.Access.Provider.SubTitle}
+                  >
+                    <Select
+                      aria-label={Locale.Settings.Access.Provider.Title}
+                      value={accessStore.provider}
+                      onChange={(e) => {
+                        accessStore.update(
+                          (access) =>
+                            (access.provider = e.target
+                              .value as ServiceProvider),
+                        );
+                      }}
+                    >
+                      {Object.entries(ServiceProvider).map(([k, v]) => (
+                        <option value={v} key={k}>
+                          {k}
+                        </option>
+                      ))}
+                    </Select>
+                  </ListItem> */}
+
+                  {openAIConfigComponent}
+                  {/* {azureConfigComponent}
+                  {googleConfigComponent}
+                  {anthropicConfigComponent}
+                  {baiduConfigComponent}
+                  {byteDanceConfigComponent}
+                  {alibabaConfigComponent}
+                  {tencentConfigComponent}
+                  {moonshotConfigComponent}
+                  {stabilityConfigComponent}
+                  {lflytekConfigComponent} */}
+                </>
+              }
+            </>
+          )}
+
+          {/* <ListItem
+            title={Locale.Settings.Access.CustomModel.Title}
+            subTitle={Locale.Settings.Access.CustomModel.SubTitle}
+          >
+            <input
+              aria-label={Locale.Settings.Access.CustomModel.Title}
+              type="text"
+              value={config.customModels}
+              placeholder="model1,model2,model3"
+              onChange={(e) =>
+                config.update(
+                  (config) => (config.customModels = e.currentTarget.value),
+                )
+              }
+            ></input>
+          </ListItem> */}
+        </List>
+
         <List>
           <ListItem title={Locale.Settings.Avatar}>
             <Popover
@@ -1454,7 +1522,7 @@ export function Settings() {
           </ListItem> */}
         </List>
 
-        <SyncItems />
+        {/* <SyncItems /> */}
 
         <List>
           <ListItem
@@ -1475,7 +1543,7 @@ export function Settings() {
             ></input>
           </ListItem>
 
-          <ListItem
+          {/* <ListItem
             title={Locale.Settings.Mask.Builtin.Title}
             subTitle={Locale.Settings.Mask.Builtin.SubTitle}
           >
@@ -1490,10 +1558,10 @@ export function Settings() {
                 )
               }
             ></input>
-          </ListItem>
+          </ListItem> */}
         </List>
 
-        <List>
+        {/* <List> */}
           {/* <ListItem
             title={Locale.Settings.Prompt.Disable.Title}
             subTitle={Locale.Settings.Prompt.Disable.SubTitle}
@@ -1511,7 +1579,7 @@ export function Settings() {
             ></input>
           </ListItem> */}
 
-          <ListItem
+          {/* <ListItem
             title={Locale.Settings.Prompt.List}
             subTitle={Locale.Settings.Prompt.ListCount(
               builtinCount,
@@ -1525,75 +1593,9 @@ export function Settings() {
               onClick={() => setShowPromptModal(true)}
             />
           </ListItem>
-        </List>
-
-        {/* <List id={SlotID.CustomModel}>
-          {accessCodeComponent}
-
-          {!accessStore.hideUserApiKey && (
-            <>
-              {useCustomConfigComponent}
-
-              {
-                <>
-                  <ListItem
-                    title={Locale.Settings.Access.Provider.Title}
-                    subTitle={Locale.Settings.Access.Provider.SubTitle}
-                  >
-                    <Select
-                      aria-label={Locale.Settings.Access.Provider.Title}
-                      value={accessStore.provider}
-                      onChange={(e) => {
-                        accessStore.update(
-                          (access) =>
-                            (access.provider = e.target
-                              .value as ServiceProvider),
-                        );
-                      }}
-                    >
-                      {Object.entries(ServiceProvider).map(([k, v]) => (
-                        <option value={v} key={k}>
-                          {k}
-                        </option>
-                      ))}
-                    </Select>
-                  </ListItem>
-
-                  {openAIConfigComponent}
-                  {azureConfigComponent}
-                  {googleConfigComponent}
-                  {anthropicConfigComponent}
-                  {baiduConfigComponent}
-                  {byteDanceConfigComponent}
-                  {alibabaConfigComponent}
-                  {tencentConfigComponent}
-                  {moonshotConfigComponent}
-                  {stabilityConfigComponent}
-                  {lflytekConfigComponent}
-                </>
-              }
-            </>
-          )}
-
-          <ListItem
-            title={Locale.Settings.Access.CustomModel.Title}
-            subTitle={Locale.Settings.Access.CustomModel.SubTitle}
-          >
-            <input
-              aria-label={Locale.Settings.Access.CustomModel.Title}
-              type="text"
-              value={config.customModels}
-              placeholder="model1,model2,model3"
-              onChange={(e) =>
-                config.update(
-                  (config) => (config.customModels = e.currentTarget.value),
-                )
-              }
-            ></input>
-          </ListItem>
         </List> */}
 
-        <List>
+        {/* <List>
           <ModelConfigList
             modelConfig={config.modelConfig}
             updateConfig={(updater) => {
@@ -1602,7 +1604,7 @@ export function Settings() {
               config.update((config) => (config.modelConfig = modelConfig));
             }}
           />
-        </List>
+        </List> */}
 
         {shouldShowPromptModal && (
           <UserPromptModal onClose={() => setShowPromptModal(false)} />
